e35d7480f07976c62c5247ae883ac18c01617f3f
music-traning <nk175jp@gmail.com>
2026-04-20 18:14:38 +0900

    OGP画像とメタタグを追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
       <App />
+      <Analytics />
     </AppProvider>
   </StrictMode>,
 )
